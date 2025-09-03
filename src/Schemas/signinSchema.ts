3140246635ff63b0e4c7daadb14ee655a08d4141
SWAYAMJAISWAL <jaiswalswayam.jaiswal@gmail.com>
2025-09-04 01:55:46 +0530

    sign-in schema debug
    sign-in schema debug

--- src/Schemas/signinSchema.ts
+++ src/Schemas/signinSchema.ts
@@ -1,7 +1,7 @@
-import {z} from "zod"
+import * as z from "zod"
 
 export const signinSchema=z.object({
-    usename:z.string().length(2,"usrname is atleast of 2 length"),
+    identifier:z.string().min(2," Username or Email must be at least 2 characters long").max(50,"Username or Email must be less than 50 characters long"),
     password:z.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{6,}$/),
-    email:z.string().email("Invalid email address")
+    
 })
